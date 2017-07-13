18058a939d9af4b1a62af9da58d14cb1e3c6e4f2
MSeanG <sgilmore@michael-sean.com>
2017-07-12 21:41:51 -0400

    minor changes to hangman, engine and layout

---
+++ public/script/engine.js
@@ -0,0 +1,12 @@
+// display secret word as list of hidden letters
+/* Write a Hangman game that displays a secret word as a list of hidden letters. Also display an alphabet of letters that the user can guess while trying to guess the word. After a letter is used, disable that letter from the alphabet so the user will not try to guess the same letter twice. When a guess matches one or more letters in the secret word, reveal those letters to the user. Keep track of the number of incorrect guesses and end the game if the number of incorrect guesses reaches 6. If the user reveals all of the hidden letters before reaching 6 incorrect guesses, then the user wins. */
+//var letters = $('#glyphs');
+//console.log(letters);
+// for (var i = 0; i < 26; i++) {
+//     var newletter = $('<a>')
+//}
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+//console.log(alphabet);
+for (var i = 0; i < alphabet.length; i++){
+    $('#glyphs').append('<a>' + alphabet[i]);
+}
